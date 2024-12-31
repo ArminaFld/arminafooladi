dbbe6a6a2d1a9433d369c3cd4f1db52b191274c6
Pc.unique <armina.fld2005@gmail.com>
2024-12-31 12:10:54 +0330

    project added

---
+++ .idea/src/MyApp/GUI.java
@@ -0,0 +1,11 @@
+package MyApp;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GUI {
+    private final Scanner scannerInt = new Scanner(System.in);
+    private final Scanner scannerStr = new Scanner(System.in);
+    private final Scanner scannerDou = new Scanner(System.in);
+
+
+}
---
+++ .idea/src/MyApp/Main.java
@@ -0,0 +1,15 @@
+package MyApp;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Hospital");
+        frame.setSize(500,500);
+
+
+
+
+    }
+}
